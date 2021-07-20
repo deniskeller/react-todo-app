4cffd7e0da2abe69c397736a7045e7c704283e5d
deniskeller <dentean@mail.ru>
2021-07-20 17:55:10 +0400

    add .env

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import 'firebase/firestore';
 import 'firebase/database';
 
 firebase.initializeApp({
-  apiKey: 'AIzaSyCg8V6GntuBq8v8tjReYSJqtnihK7ICmek',
-  authDomain: 'react-todo-app-e42cf.firebaseapp.com',
-  databaseURL: 'https://react-todo-app-e42cf.firebaseio.com',
-  projectId: 'react-todo-app-e42cf',
-  storageBucket: 'react-todo-app-e42cf.appspot.com',
-  messagingSenderId: '1023324535270',
-  appId: '1:1023324535270:web:f9f5d6db0425e710bd7e27',
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 });
 
 const db = firebase.firestore();
