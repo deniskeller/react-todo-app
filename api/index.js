8fdb3d4b298cd352ee3d149e81f6f98bcf40e2de
deniskeller <dentean@mail.ru>
2025-04-24 16:58:38 +0400

    Fix json-server dependency

--- api/index.js
+++ api/index.js
@@ -1,13 +1,16 @@
 import jsonServer from 'json-server';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const server = jsonServer.create();
-const router = jsonServer.router('../db.json');
+const router = jsonServer.router(path.join(__dirname, '../db.json'));
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
 server.use(router);
-server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*'); // Разрешаем CORS
-  next();
+server.listen(process.env.PORT || 5000, () => {
+  console.log('JSON Server is running');
 });
 
 export default server;
--- client/src/App.tsx
+++ client/src/App.tsx
@@ -21,6 +21,12 @@ const App: React.FC = () => {
     if (location.pathname === '/') navigate('/page/1');
   }, [location.pathname, navigate]);
 
+  // useEffect(() => {
+  //   fetch('https://dk-react-todo-app.vercel.app/api/todos')
+  //     .then((response) => response.json())
+  //     .then((data) => console.log(data));
+  // }, []);
+
   return (
     <Routes>
       <Route path='/' element={<TodoList />} />
